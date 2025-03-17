531bcdd347fbf21fa79963fef1998a3bfdc28ad1
Danil Gordeev <dan.gor220@yandex.ru>
2025-03-17 16:24:29 +0300

    feat: work with recpatcha for contacts component

--- api/verify-captcha.ts
+++ api/verify-captcha.ts
@@ -1,25 +1,32 @@
 export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
   const { captchaToken } = req.body;
   const secretKey = process.env.RECAPTCHA_SECRET_KEY;
 
   if (!secretKey) {
-    throw new Error('RECAPTCHA_SECRET_KEY is not defined');
+    return res.status(500).json({ message: 'RECAPTCHA_SECRET_KEY is not defined' });
   }
 
-  const googleResponse = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
-    method: 'POST',
-    body: new URLSearchParams({
-      secret: secretKey,
-      response: captchaToken,
-    }),
-  });
+  try {
+    const googleResponse = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+      method: 'POST',
+      body: new URLSearchParams({
+        secret: secretKey,
+        response: captchaToken,
+      }),
+    });
 
-  const googleResult = await googleResponse.json();
+    const googleResult = await googleResponse.json();
 
-  if (googleResult.success) {
-    return res.status(200).json({ success: true });
-  } else {
-    // Если ошибка валидации
-    return res.status(400).json({ success: false });
+    if (googleResult.success) {
+      return res.status(200).json({ success: true });
+    } else {
+      return res.status(400).json({ success: false, error: googleResult['error-codes'] });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'Server Error', error: error.message });
   }
 }
