06412d0cecd811f49d6ea0203e1e793830e6e56b
Danil Gordeev <dan.gor220@yandex.ru>
2025-03-28 16:45:03 +0300

    feat: add autoprefixer to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import autoprefixer from 'autoprefixer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
