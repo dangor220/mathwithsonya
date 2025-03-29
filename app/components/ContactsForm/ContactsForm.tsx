dcbef87027c50b72026562297123508b785ef09e
Danil Gordeev <dan.gor220@yandex.ru>
2025-03-29 16:07:57 +0300

    fix: api to verify captcha

--- app/components/ContactsForm/ContactsForm.tsx
+++ app/components/ContactsForm/ContactsForm.tsx
@@ -63,7 +63,7 @@ export default function ContactsForm({ content }: { content: DefaultContent }): 
     setMessageStatus(MessageStatus.Loading);
 
     try {
-      const recaptchaResponse = await fetch('/api/verify-captcha', {
+      const recaptchaResponse = await fetch('/api/route', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
