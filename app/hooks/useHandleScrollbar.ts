b85d91476f446178c153190649404b286d540ff6
Danil Gordeev <dan.gor220@yandex.ru>
2025-04-02 14:06:50 +0300

    fix: change logic for useHandleScrollbar

--- app/hooks/useHandleScrollbar.ts
+++ app/hooks/useHandleScrollbar.ts
@@ -1,44 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 
 export default function useHandleScrollbar(
   headerRef: React.RefObject<HTMLDivElement | null>,
   open: boolean,
 ) {
-  useEffect(() => {
-    const hasScrollbar = () => {
-      return document.body.scrollHeight > window.innerHeight;
-    };
-    document.body.style.overflow = open ? 'hidden' : '';
+  const getScrollbarWidth = useCallback(() => {
+    const div = document.createElement('div');
+    div.style.width = '100px';
+    div.style.height = '100px';
+    div.style.overflow = 'scroll';
+    div.style.position = 'absolute';
+    div.style.top = '-9999px';
+    document.body.appendChild(div);
+    const scrollbarWidth = div.offsetWidth - div.clientWidth;
+    document.body.removeChild(div);
+    return scrollbarWidth;
+  }, []);
 
-    const getScrollbarWidth = () => {
-      const div = document.createElement('div');
-      div.style.width = '100px';
-      div.style.height = '100px';
-      div.style.overflow = 'scroll';
-      div.style.position = 'absolute';
-      div.style.top = '-9999px';
-      document.body.appendChild(div);
-      const scrollbarWidth = div.offsetWidth - div.clientWidth;
-      document.body.removeChild(div);
-      return scrollbarWidth;
-    };
+  const hasScrollbar = () => document.body.scrollHeight > window.innerHeight;
+
+  useEffect(() => {
     if (!headerRef.current || !hasScrollbar()) {
       document.body.style.cssText = '';
+      document.documentElement.style.cssText = '';
       return;
     }
 
-    if (open) {
-      const scrollbarWidth = getScrollbarWidth();
+    const scrollbarWidth = getScrollbarWidth();
 
+    if (open) {
       document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
       document.body.style.paddingRight = `${scrollbarWidth}px`;
 
       const headerWidth = headerRef.current.getBoundingClientRect().width - scrollbarWidth;
       headerRef.current.style.width = `${headerWidth}px`;
     } else {
       document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
       document.body.style.paddingRight = '';
-      headerRef.current.style.width = `100%`;
+      headerRef.current.style.width = '100%';
     }
-  }, [open, headerRef]);
+  }, [open, headerRef, getScrollbarWidth]);
 }
