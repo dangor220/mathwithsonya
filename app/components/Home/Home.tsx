b5b5ef24d3d56941617c3728ee1cb14ce0aec209
Danil Gordeev <dan.gor220@yandex.ru>
2025-04-07 00:46:51 +0300

    feat: optimization home component

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -16,7 +16,12 @@ import { useIsMobile } from '@/app/hooks/useIsMobile';
 export default function Home({ content }: { content: DefaultContent }): React.ReactElement {
   const isMobile = useIsMobile();
   const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0, 0.5], [1, isMobile ? 1.4 : 1.18]);
+  const scale = useTransform(
+    scrollYProgress,
+    [0, isMobile ? 0.2 : 0.5],
+    [1, isMobile ? 1.05 : 1.18],
+  );
+
   const scaleQuote = useTransform(scrollYProgress, [0, 0.5], [1, isMobile ? 1.6 : 1.18]);
   const y = useTransform(scrollYProgress, [0, 0.5], [0, -400]);
   const translateY = useTransform(y, (value) => `translateY(${value}px)`);
@@ -27,7 +32,7 @@ export default function Home({ content }: { content: DefaultContent }): React.Re
         <motion.h1
           style={{ transform: translateY }}
           initial={{ opacity: 0 }}
-          animate={{ opacity: 1, transition: { duration: 2, delay: 0.5 } }}
+          animate={{ opacity: 1, transition: { duration: 2 } }}
           className={styles.title}>
           <span className={styles.name}>{content.name}</span>
           <span className={styles.surname}>{content.surname}</span>
@@ -44,7 +49,6 @@ export default function Home({ content }: { content: DefaultContent }): React.Re
               }}
               transition={{
                 duration: 1,
-                delay: 1,
                 rotate: {
                   duration: 3,
                   ease: 'easeInOut',
@@ -58,11 +62,11 @@ export default function Home({ content }: { content: DefaultContent }): React.Re
             <motion.div
               style={{ scale }}
               initial={{ opacity: 0, y: 50 }}
-              animate={{ opacity: 1, y: 0, transition: { duration: 1, delay: 0.5 } }}>
+              animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}>
               <Image
                 className={styles.image}
                 src={teacher}
-                quality={65}
+                quality={60}
                 sizes="(max-width: 768px) 80vw, (max-width: 1200px) 50vw, 992px"
                 alt="Софья Герасимова"
                 priority
@@ -72,11 +76,23 @@ export default function Home({ content }: { content: DefaultContent }): React.Re
               style={{ scale: scaleQuote }}
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1, delay: 1 }}
+              transition={{ duration: 1 }}
               className={styles.items}>
-              <Image className={styles.note} src={note} loading="lazy" alt="Тетрадь" />
-              <Image className={styles.calc} src={calc} loading="lazy" alt="Калькулятор" />
-              <Image className={styles.ruler} src={ruler} loading="lazy" alt="Линейка" />
+              <Image className={styles.note} src={note} quality={50} loading="lazy" alt="Тетрадь" />
+              <Image
+                className={styles.calc}
+                src={calc}
+                quality={50}
+                loading="lazy"
+                alt="Калькулятор"
+              />
+              <Image
+                className={styles.ruler}
+                src={ruler}
+                quality={50}
+                loading="lazy"
+                alt="Линейка"
+              />
             </motion.div>
           </div>
         </div>
