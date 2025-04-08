f339e9e08a637e8ab93e257d452a880eebc69697
Danil Gordeev <dan.gor220@yandex.ru>
2025-04-08 13:21:45 +0300

    fix: transfer block styles to head with preload attr

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+
+export default function HeadComponent() {
+  return (
+    <Head>
+      <link rel="preload" href="/_next/static/css/b7862c6f00372e3f.css" as="style" />
+    </Head>
+  );
+}
