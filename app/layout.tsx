1030fbb0f20bf8035c7ef2f798b7c8a486db4223
Danil Gordeev <dan.gor220@yandex.ru>
2025-04-08 17:06:22 +0300

    feat: try remove blocking css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,31 @@
-// import 'normalize.css';
-// import './styles/globals.scss';
-import { Montserrat, Marck_Script, Bona_Nova_SC } from 'next/font/google';
+import 'normalize.css';
+import './styles/globals.scss';
+import { Montserrat } from 'next/font/google';
 import type { Metadata } from 'next';
 
 const montserrat = Montserrat({
-  weight: ['400', '500', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin', 'cyrillic'],
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['cyrillic'],
   variable: '--font-montserrat',
   display: 'swap',
 });
-const marckScript = Marck_Script({
-  weight: ['400'],
-  style: ['normal'],
-  subsets: ['latin'],
-  variable: '--font-marck-script',
-  display: 'swap',
-});
-const bonaNovaSC = Bona_Nova_SC({
-  weight: ['400', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  variable: '--font-bona-nova-sc',
-  display: 'swap',
-});
+const marckScript = montserrat;
+const bonaNovaSC = montserrat;
+// const marckScript = Marck_Script({
+//   weight: ['400'],
+//   style: ['normal'],
+//   subsets: ['latin'],
+//   variable: '--font-marck-script',
+//   display: 'swap',
+// });
+// const bonaNovaSC = Bona_Nova_SC({
+//   weight: ['400', '700'],
+//   style: ['normal'],
+//   subsets: ['latin'],
+//   variable: '--font-bona-nova-sc',
+//   display: 'swap',
+// });
 
 export const metadata: Metadata = {
   title: 'Sonya and Math',
