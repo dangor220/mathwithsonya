88731e503a1277554c1db47e19a4c8c97e3d0a25
Danil Gordeev <dan.gor220@yandex.ru>
2025-04-08 14:10:42 +0300

    feat: optimize css

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: { optimizeCss: true },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [320, 420, 768, 1024, 1200, 1920],
