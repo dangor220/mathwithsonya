27e7c0adc26360bc20d745145761bead4afa5639
Danil Gordeev <dan.gor220@yandex.ru>
2025-04-08 16:40:43 +0300

    feat: try remove blocking css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-// import './styles/globals.scss';
+import 'normalize.css';
+import './styles/globals.scss';
 import { Montserrat, Marck_Script, Bona_Nova_SC } from 'next/font/google';
 import type { Metadata } from 'next';
 
@@ -41,49 +42,6 @@ export default function RootLayout({
     <html
       lang="ru"
       className={`${montserrat.variable} ${marckScript.variable} ${bonaNovaSC.variable}`}>
-      <head>
-        <style>{`
-         :root {
-            font-family: var(--font-montserrat), sans-serif;
-            line-height: 1.5;
-            font-weight: 400;
-            font-size: 62.5%;
-            font-synthesis: none;
-            text-rendering: optimizeLegibility;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-            -webkit-tap-highlight-color: transparent;
-          }
-
-          *,
-          *::before,
-          *::after {
-            padding: 0;
-            margin: 0;
-            list-style: none;
-            text-decoration: none;
-            border: none;
-            box-sizing: border-box;
-          }
-
-          html,
-          body {
-            max-width: 100vw;
-            overflow-x: hidden;
-          }
-
-          h1 {
-            padding: 0;
-            margin: 0;
-          }
-
-          .container {
-            max-width: 1280px;
-            margin: 0 auto;
-            padding: 0 2.5rem;
-          }
-        `}</style>
-      </head>
       <body>{children}</body>
     </html>
   );
