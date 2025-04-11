cf025da5e559283bc31cb311dab66a1b1501081c
Danil Gordeev <dan.gor220@yandex.ru>
2025-04-11 13:44:21 +0300

    feat: add loader component

--- app/components/ClientApp/ClientApp.tsx
+++ app/components/ClientApp/ClientApp.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
@@ -7,12 +7,24 @@ import Main from '../Main/Main';
 
 import { DefaultContent } from '@/app/types/defaultContentTypes';
 import { useIsMobile } from '@/app/hooks/useIsMobile';
+import Loading from '../Loading/Loading';
 
 export default function ClientApp({ content }: { content: DefaultContent }) {
+  const [loading, setLoading] = useState(true);
   const headerRef = useRef(null);
   useIsMobile();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
+      {loading && <Loading loading={loading} />}
       <Header content={content} ref={headerRef} />
       <Main content={content} headerRef={headerRef} />
       <Footer content={content} />
---
+++ app/components/Loading/Loading.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './Loading.module.scss';
+
+export default function Loading({ loading }: { loading: boolean }) {
+  return (
+    <div className={`${styles.loaderWrapper} ${!loading ? styles.hidden : ''}`}>
+      <div className={styles.loader}>
+        <div className={styles.imageWrapper}>
+          <Image
+            className={styles.image}
+            src={'/images/loading/loading.webp'}
+            fill
+            alt="loading"
+            unoptimized
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
