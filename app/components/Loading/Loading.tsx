7141f292d157391d8596713df089e4c76a4cf9bb
Danil Gordeev <dan.gor220@yandex.ru>
2025-04-15 01:23:23 +0300

    feat: improve optimization

--- app/components/Loading/Loading.tsx
+++ app/components/Loading/Loading.tsx
@@ -2,21 +2,30 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './Loading.module.scss';
 
+import { LazyMotion, domAnimation } from 'motion/react';
+import * as m from 'motion/react-m';
+
 export default function Loading({ loading }: { loading: boolean }) {
   return (
-    <div className={`${styles.loaderWrapper} ${loading ? styles.hidden : ''}`}>
-      <div className={styles.loader}>
-        <div className={styles.imageWrapper}>
-          <Image
-            className={styles.image}
-            src={'/images/loading/loading.webp'}
-            fill
-            priority
-            alt="loading"
-            unoptimized
-          />
+    <LazyMotion features={domAnimation}>
+      <m.div
+        initial={{ opacity: 1, scale: 1 }}
+        animate={loading ? { opacity: 0, scale: 1.02 } : { opacity: 1, scale: 1 }}
+        transition={{ duration: 0.4 }}
+        className={styles.loaderWrapper}>
+        <div className={styles.loader}>
+          <div className={styles.imageWrapper}>
+            <Image
+              className={styles.image}
+              src={'/images/loading/loading.webp'}
+              fill
+              priority
+              alt="Загрузка..."
+              unoptimized
+            />
+          </div>
         </div>
-      </div>
-    </div>
+      </m.div>
+    </LazyMotion>
   );
 }
