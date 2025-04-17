784dc3402de67bd3cf0575f2da726457d3a8d134
Danil Gordeev <dan.gor220@yandex.ru>
2025-04-18 02:11:49 +0300

    feat: check reviews data

--- app/components/DynamicSwiper/DynamicSwiper.tsx
+++ app/components/DynamicSwiper/DynamicSwiper.tsx
@@ -15,46 +15,53 @@ import 'swiper/scss/autoplay';
 import { DefaultContent } from '@/app/types/defaultContentTypes';
 
 export default function DynamicSwiper({ content }: { content: DefaultContent }) {
+  const reviews = content.reviews.list || [];
   return (
-    <Swiper
-      pagination={{ clickable: true }}
-      modules={[Pagination, A11y]}
-      breakpoints={{
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-        1024: {
-          slidesPerView: 2,
-          spaceBetween: 50,
-        },
-      }}>
-      {content.reviews.list.map((item, id) => (
-        <SwiperSlide key={id}>
-          <div className={styles.card}>
-            <div className={styles.user}>
-              <Image
-                className={styles.image}
-                src={item.image}
-                alt={item.name}
-                width={80}
-                height={80}
-              />
-            </div>
-            <div className={styles.review}>
-              <div className={styles.text}>{`"${item.review}"`}</div>
-              <div className={styles.info}>
-                <div className={styles.name}>{item.name + ','} </div>
-                <div className={styles.comment}>{item.about}</div>
+    reviews && (
+      <Swiper
+        pagination={{ clickable: true }}
+        modules={[Pagination, A11y]}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+        }}>
+        {reviews.map((item, id) => (
+          <SwiperSlide key={id}>
+            <div className={styles.card}>
+              {item.image && (
+                <div className={styles.user}>
+                  <Image
+                    className={styles.image}
+                    src={item.image}
+                    alt={item.name}
+                    width={80}
+                    height={80}
+                  />
+                </div>
+              )}
+              <div className={styles.review}>
+                {item.review && <div className={styles.text}>{`"${item.review}"`}</div>}
+                {(item.name || item.about) && (
+                  <div className={styles.info}>
+                    {item.name && <div className={styles.name}>{item.name + ','}</div>}
+                    {item.about && <div className={styles.comment}>{item.about}</div>}
+                  </div>
+                )}
               </div>
             </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    )
   );
 }
--- app/components/Reviews/Reviews.tsx
+++ app/components/Reviews/Reviews.tsx
@@ -20,38 +20,48 @@ type Props = {
 };
 
 export default function Reviews({ content, scrollDirection }: Props): React.ReactElement {
+  const reviews = content.reviews || {};
+  const list = reviews.list || [];
+
+  if (Object.values(reviews).every((value) => !value) || list.length === 0) return <></>;
+
   return (
     <section className={styles.reviews} id="reviews">
       <div className={`container ${styles.wrapper}`}>
         <LazyMotion features={domAnimation}>
-          <m.h2
-            initial={scrollDirection === 'down' ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
-            whileInView={{
-              opacity: 1,
-              y: 0,
-              transition: {
-                type: 'spring',
-                bounce: 0.4,
-                duration: 1,
-              },
-            }}
-            className={styles.title}>
-            {content.reviews.title}
-          </m.h2>
-          <m.div
-            initial={scrollDirection === 'down' ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
-            whileInView={{
-              opacity: 1,
-              y: 0,
-              transition: {
-                type: 'spring',
-                bounce: 0.4,
-                duration: 1,
-              },
-            }}
-            className={styles.content}>
-            <DynamicSwiper content={content} />
-          </m.div>
+          {reviews.title && (
+            <m.h2
+              initial={scrollDirection === 'down' ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  type: 'spring',
+                  bounce: 0.4,
+                  duration: 1,
+                },
+              }}
+              className={styles.title}>
+              {reviews.title}
+            </m.h2>
+          )}
+
+          {list && (
+            <m.div
+              initial={scrollDirection === 'down' ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  type: 'spring',
+                  bounce: 0.4,
+                  duration: 1,
+                },
+              }}
+              className={styles.content}>
+              <DynamicSwiper content={content} />
+            </m.div>
+          )}
         </LazyMotion>
       </div>
     </section>
