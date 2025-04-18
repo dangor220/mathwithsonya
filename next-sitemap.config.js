9e402d47a0660f172982f92b9e62f15c7327bd73
Danil Gordeev <dan.gor220@yandex.ru>
2025-04-18 12:46:39 +0300

    feat: work with sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   siteUrl: 'https://www.mathwithsonya.ru',
   generateRobotsTxt: true,
+  exclude: [
+    '/favicon.ico',
+    '/manifest.json',
+    '/apple-icon.png',
+    '/icon0.svg',
+    '/icon1.png',
+    '/web-app-manifest-192x192.png',
+    '/web-app-manifest-512x512.png',
+  ],
 };
